e2f516c4c3be9616f5daafe53f24aa03ff5d0117
jhnpaolotaguinod <jhnpaolotaguinod@gmail.com>
2026-04-09 22:02:59 +0800

    Update Data_Tables.java

--- Assesment-Project/src/test/java/Requirement_3/Data_Tables.java
+++ Assesment-Project/src/test/java/Requirement_3/Data_Tables.java
@@ -3,16 +3,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 public class Data_Tables {
     static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        // driver.get("https://the-internet.herokuapp.com");
 
         try {
             driver.get("https://the-internet.herokuapp.com/");
